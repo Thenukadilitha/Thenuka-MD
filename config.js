ba6aeeb3ae66ca2549ae334b0a6417c97650da11
Thenukadilitha <thenukadilitha123@gmail.com>
2025-11-18 06:23:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,19 @@
  * @description : A Multi-functional whatsapp bot script.
  * @version 3.0.0
  **/
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+
+MONGODB: process.env.MONGODB || "mongodb://mongo:OZTIvyPWIpiJXVdNQwGOFbBofpfALZNP@switchyard.proxy.rlwy.net:35776",
+GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
+tokenm: process.env.GITHUB_TOKEN || "false",
+repo: process.env.GITHUB_REPO || "Thenuka-MD",
+}
 
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
